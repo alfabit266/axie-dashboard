01c0f7fa2deb101a1182f240a69d8206eb04c975
alfabit266 <73493186+alfabit266@users.noreply.github.com>
2025-11-28 03:13:31 -0300

    Update route.js

--- app/route.js
+++ app/route.js
@@ -1,1 +1,19 @@
+export async function GET(req) {
+  const url = req.nextUrl.searchParams.get("url");
 
+  if (!url) {
+    return new Response(JSON.stringify({ error: "URL missing" }), { status: 400 });
+  }
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: "Erro ao acessar API" }), { status: 500 });
+  }
+}
